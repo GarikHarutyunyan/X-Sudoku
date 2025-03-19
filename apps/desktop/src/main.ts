0861081df132b9092bf1cc4c465332fd478967da
Garik <garik.harutyunian@gmail.com>
2025-03-20 01:08:59 +0400

    Set up desktop app with Electron JS

---
+++ apps/desktop/src/main.ts
@@ -0,0 +1,25 @@
+import {app, BrowserWindow} from 'electron';
+
+let mainWindow;
+
+app.whenReady().then(() => {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadURL('http://localhost:3000');
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
